a9a7d2dda5775bb7f7bb574721d930f675444293
Louis Bouchard <lbouchard@online.net>
2018-07-09 11:50:17 +0200

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@
 set -e
 
 deploy_myenv() {
-  rm -Rf ~/.vim ~/.vimrc
-  ln -s dot.vimrc ~/.vimrc
+  apt -y install rake
+  rm -Rf ~/.vim ~/.vimrc ~/.bash_profile ~/.tmux.conf ~/.gvimrc.after 
   ln -s dot.bash_profile ~/.bash_profile
   ln -s dot.gvimrc.after ~/.gvimrc.after
   ln -s dot.tmux.conf ~/.tmux.conf
-  ln -s dot.vimrc ~/.vimrc
   curl -L https://bit.ly/janus-bootstrap | bash
   }
 
-  deploy_myenv
+  if ! [ -d ~/.vim/janus ];do
+    deploy_myenv
+  done
