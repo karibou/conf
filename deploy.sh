f2543b8408005d7c24fad3f43993a22cb4054210
Louis Bouchard <lbouchard@online.net>
2018-07-09 12:24:06 +0200

    Add SyntasticCheck

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,11 @@ deploy_myenv() {
   ln -s dot.gvimrc.after ~/.gvimrc.after
   ln -s dot.tmux.conf ~/.tmux.conf
   curl -L https://bit.ly/janus-bootstrap | bash
+  # install Pathogen
+  mkdir -p ~/.vim/autoload ~/.vim/bundle
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+  cd ~/.vim/bundle && \
+    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
   }
 
   if ! [ -d ~/.vim/janus ];do
